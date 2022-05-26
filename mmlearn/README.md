53bd351784619871a5c98bd40b406efa2d31341c
MatejBevec <40042371+MatejBevec@users.noreply.github.com>
2022-05-26 10:17:29 +0200

    Added dev TODOs as README file

---
+++ mmlearn/README.md
@@ -0,0 +1,60 @@
+# Dev Whiteboard
+
+## To-Do
+- ~~look up docstring and static typing conventions~~ ✓
+- ~~feature extractor interface spec~~ ✓
+- ~~implement image feature extractors~~ ✓
+- ~~implement MultimodalDataset class~~ ✓
+- ~~model interface spec~~ ✓
+- ~~look at torch lightning interface~~ ✓ (not applicable)
+- text feature extractors (see issues):
+  - keyword features
+  - POS n-grams
+  - ~~doc2vec~~ ✓
+- ~~trad text classifier~~ ✓
+- ~~fine-tuned X image classifier~~ ✓
+- ~~dataset downloads from dropbox~~ ✓
+- ~~global global constants~~ ✓
+- ~~util class (log, hidden print....)~~ ✓
+- ~~eval module~~ ✓
+  - ~~holdout(model, dataset, metric)~~ ✓
+  - ~~cross_validate(model, dataset, metric)~~ ✓
+  - ~~eval-all~~ ✓
+- ~~basic mm models: late fusion, early fusion with concat~~ ✓
+- ~~extend MultimodalDataset interface for audio and video~~ ✓
+- research types of early fusion
+- create dataset from torch dataset (constructor or factory?)
+- verbose mode
+- models should return probabilites also
+- sound feature extractors
+- video feature extractors
+- more image models
+- docs from docstrings
+
+
+## Considerations
+- wrap Pytorch NN classifiers and "auto reg" sklearn classifiers into sklearn-like classes
+- think about the module naming (fe.image and image might clash)
+  - yes, use full names ✓
+- are mutable default arguments a problem?
+  - yes, use "default" string ✓
+- TODO: how many separate pre-configured classifiers should be provided?
+- support for when embedding space or text space is larger than memory?
+  - no
+- TODO: get_transformed(fes) in MultiModal dataset?
+- TODO: helper train functions or separate Classifier, NeuralClassifier classes?
+- is one MultimodalDataset for all modalities clean, or should i split the class?
+  - one dataset
+- TODO: JSON file for settings
+- TODO: support regression?
+
+## Style Guide
+  - import default modules, external libraries, internal modules in this order
+  - use underscores for _internal_functions
+  - Google style docstrings
+  - no dashes in module names
+  - "image", "text", "fe", "mm" refer to image, text, feature extractors and multimodal in names
+  - "targets" or "labels" refer to class values, "classes" refer to class types
+  - use util.log_progress(string) to log function/model progress
+  - do not use mutables in DEFAULT arguments (don't do "model=MobileNetV3()")
+  - TODO: do not import specific classes or functions, call module.Class() instead
--- mmlearn/main.py
+++ mmlearn/main.py
@@ -1,59 +1,5 @@
 # SCRATCH PAPER FOR TESTING MODULES
 
-# To-Do:
-# TODO: look up docstring and static typing conventions ✓
-# TODO: feature extractor interface spec ✓
-# TODO: implement image feature extractors ✓
-# TODO: create dataset from torch dataset (constructor or factory?)
-# TODO: implement MultimodalDataset class ✓
-# TODO: model interface spec ✓
-# TODO: look at torch lightning interface ✓ (not applicable)
-# TODO: text feature extractors:
-#   - keyword features
-#   - POS n-grams
-#   - doc2vec ✓
-# TODO: trad text classifier ✓
-# TODO: fine-tuned X image classifier ✓
-# TODO: dataset downloads from dropbox ✓
-# TODO: research types of early fusion
-# TODO: global global constants ✓
-# TODO: util class (log, hidden print....) ✓
-# TODO: eval module ✓
-#   - holdout(model, dataset, metric) ✓
-#   - cross_validate(model, dataset, metric) ✓
-#   - eval-all ✓
-# TODO: basic mm models: late fusion, early fusion with concat ✓
-# TODO: verbose mode
-# TODO: should models return probabilites as well?
-# TODO: extend MultimodalDataset interface for audio and video
-
-# Considerations:
-# TODO: wrap Pytorch NN classifiers and "auto reg" sklearn classifiers into sklearn-like classes!!!!
-# TODO: think about the module naming (fe.image and image might clash) !!!
-#   - yes, use full names
-# are mutable default arguments a problem?
-#   - yes, use "default" string
-# TODO: how many separate pre-configured classifiers should be provided?
-# support for when embedding space or text space is larger than memory?
-#   - no
-# TODO: get_transformed(fes) in MultiModal dataset?
-# TODO: helper train functions or separate Classifier, NeuralClassifier classes?
-# is one MultimodalDataset for all modalities clean, or should i split the class?
-#   - one dataset
-# TODO: JSON file for settings?
-
-# Style Guide:
-#   - import default modules, external libraries, internal modules in this order
-#   - use underscores for _internal_functions
-#   - Google style docstrings
-#   - no dashes in module names
-#   - "image", "text", "fe", "mm" refer to image, text, feature extractors and multimodal in names
-#   - "targets" or "labels" refer to class values, "classes" refer to class types
-#   - use util.log_progress(string) to log function/model progress
-#   - do not use mutables in DEFAULT arguments (don't do "model=MobileNetV3()")
-#   - TODO: do not import specific classes or functions, call module.Class() instead
-
-
 import os, sys
 import numpy as np
 import torch
